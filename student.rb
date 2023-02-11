652f6dc04da1a378128dba7d25d1fa213e055514
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-02-11 16:34:18 +0300

    Add basic UI

--- student.rb
+++ student.rb
@@ -1,10 +1,11 @@
 require './person'
+require './classroom'
 
 class Student < Person
   attr_reader :classroom
 
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
