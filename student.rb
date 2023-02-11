d0d4a9ce5f7c69064a441ebcecab979bcecfea51
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-02-11 06:23:01 +0300

    Setup associations

--- student.rb
+++ student.rb
@@ -1,15 +1,19 @@
-require_relative './person'
-# inherits from Person
+require './person'
+
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
-  # play_hooky
-  def play_hooky
+  def play_hocky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
